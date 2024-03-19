fbc56d930ac75527fcbf389cb3326968b8dc9947
windchim1 <windchim1@outlook.com>
2024-03-19 10:55:25 +0800

    parse http request

--- src/request.rs
+++ src/request.rs
@@ -14,16 +14,50 @@ pub struct Request {
 }
 
 impl Request {
-    //TODO:: request parse
+    ///parse http Request
+    ///TODO::  don't  use hattparse lib,and do it by self
     pub(crate) fn parse(buffer: &[u8]) -> Result<Request, Box<dyn Error>> {
         let mut headers = [httparse::EMPTY_HEADER; 16];
         let mut req = httparse::Request::new(&mut headers);
-        match req.parse(buffer)? {
+        let res = match req.parse(buffer)? {
             httparse::Status::Complete(amt) => amt,
             httparse::Status::Partial => {
                 return Err("Request is incompete".into());
             }
         };
-        todo!()
+        let method = match req.method.ok_or("Method not found")? {
+            "GET" => HttpMethod::GET,
+            "POST" => HttpMethod::POST,
+            "PUT" => HttpMethod::PUT,
+            "DELETE" => HttpMethod::DELETE,
+            _ => HttpMethod::OTHER(req.method.unwrap().to_string()),
+        };
+        let uri = req.path.ok_or("URI not found")?.to_string();
+        let version = req.version.ok_or("Version not found")?.to_string();
+
+        let mut headers = HashMap::new();
+
+        req.headers
+            .iter()
+            .try_for_each(|header| -> Result<(), Box<dyn Error>> {
+                headers.insert(
+                    header.name.to_string(),
+                    String::from_utf8(header.value.to_vec())?,
+                );
+                Ok(())
+            })?;
+
+        let body = if res < buffer.len() {
+            Some(String::from_utf8(buffer[res..].to_vec())?)
+        } else {
+            None
+        };
+        Ok(Self {
+            method,
+            uri,
+            version,
+            headers,
+            body,
+        })
     }
 }
